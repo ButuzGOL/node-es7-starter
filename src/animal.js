59c03a681f7e4bca7512d6361c15ebbae6cc6361
ButuzGOL <ButuzGOL@gmail.com>
2016-02-28 13:48:39 +0200

    Flow type

--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,12 @@
+/* @flow */
+
 export default class Animal {
-  constructor(name) {
+  name: string;
+  constructor(name: string) {
     this.name = name;
   }
 
-  speak() {
+  speak(): number {
     return `${this.name} makes a noise.`;
   }
 }
